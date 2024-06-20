f0272ab02f9c7e48de9497abe020cb15cb651c60
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2024-06-20 05:40:57 +0200

    Manual merge for getDatabase func

--- src/gateway/opcodes/LazyRequest.ts
+++ src/gateway/opcodes/LazyRequest.ts
@@ -1,42 +1,42 @@
 /*
 	Spacebar: A FOSS re-implementation and extension of the Discord.com backend.
 	Copyright (C) 2023 Spacebar and Spacebar Contributors
-	
+
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published
 	by the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
-	
+
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU Affero General Public License for more details.
-	
+
 	You should have received a copy of the GNU Affero General Public License
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 import {
-	OPCODES,
-	Payload,
-	Send,
-	WebSocket,
-	handlePresenceUpdate,
-} from "@spacebar/gateway";
-import {
-	Channel,
-	Datasource,
-	LazyRequestSchema,
-	Member,
-	Permissions,
-	Presence,
-	Role,
-	Session,
-	User,
+	getDatabase,
 	getPermission,
 	listenEvent,
+	Member,
+	Role,
+	Session,
+	LazyRequestSchema,
+	User,
+	Presence,
 	partition,
+	Channel,
+	Permissions,
 } from "@spacebar/util";
+import {
+	WebSocket,
+	Payload,
+	handlePresenceUpdate,
+	OPCODES,
+	Send,
+} from "@spacebar/gateway";
 import murmur from "murmurhash-js/murmurhash3_gc";
 import { check } from "./instanceOf";
 
@@ -73,7 +73,8 @@ async function getMembers(guild_id: string, range: [number, number]) {
 	let members: Member[] = [];
 	try {
 		members =
-			(await Datasource?.getRepository(Member)
+			(await getDatabase()
+				?.getRepository(Member)
 				.createQueryBuilder("member")
 				.where("member.guild_id = :guild_id", { guild_id })
 				.leftJoinAndSelect("member.roles", "role")
--- src/util/entities/BaseClass.ts
+++ src/util/entities/BaseClass.ts
@@ -1,17 +1,17 @@
 /*
 	Spacebar: A FOSS re-implementation and extension of the Discord.com backend.
 	Copyright (C) 2023 Spacebar and Spacebar Contributors
-	
+
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published
 	by the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
-	
+
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU Affero General Public License for more details.
-	
+
 	You should have received a copy of the GNU Affero General Public License
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
@@ -24,9 +24,9 @@ import {
 	ObjectIdColumn,
 	PrimaryColumn,
 } from "typeorm";
-import { OrmUtils } from "../imports/OrmUtils";
-import { Datasource } from "../util/Datasource";
 import { Snowflake } from "../util/Snowflake";
+import { getDatabase } from "../util/Database";
+import { OrmUtils } from "../imports/OrmUtils";
 
 export class BaseClassWithoutId extends BaseEntity {
 	private get construct() {
@@ -34,7 +34,7 @@ export class BaseClassWithoutId extends BaseEntity {
 	}
 
 	private get metadata() {
-		return Datasource.getMetadata(this.construct);
+		return getDatabase()?.getMetadata(this.construct);
 	}
 
 	assign(props: object) {
